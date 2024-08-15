daff3c8e097bd18dc9cf15ca5c539d19c438a03f
Harsh Pagada <harshpagada@gmail.com>
2024-08-15 14:32:05 +0530

    first committed

--- db.js
+++ db.js
@@ -1,30 +1,12 @@
-// const mongoose = require ('mongoose');
-
-// const mongoURI = 'mongodb://localhost:27017';
-
-// const connectMongo =()=>{
-//     mongoose.connect(mongoURI,()=>{
-//         console.log('successfully connected');
-//     })
-// }
-
-// mongoose.connect('mongodb://localhost/mydatabase', (error) => {
-//   if (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//   } else {
-//     console.log('Connected to MongoDB');
-//   }
-// });
-
-// module.exports = connectMongo;
-
-
 const mongoose = require('mongoose');
 
+require('dotenv').config()
+
+let url = process.env.DB_KEY
 
 connectMongo= async()=> {
   try {
-    await mongoose.connect('mongodb+srv://harshpagada:oCtzKF5pFc4wVCIV@cluster1.emrs1g7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
+    await mongoose.connect(url);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
